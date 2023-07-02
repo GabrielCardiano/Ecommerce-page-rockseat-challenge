f44983660a62095f071ab568ebf99fbe4ee36c87
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-07-01 21:11:58 -0300

    feat: render product page

--- ecommerce-capputeeno/src/types/Product.ts
+++ ecommerce-capputeeno/src/types/Product.ts
@@ -1,6 +1,16 @@
+import { type } from "os";
+
 export interface Product {
     name: string,
     price_in_cents: number,
     id: string, 
     image_url: string,
+    description?: string,
+    category?: string, 
+}
+
+export interface ProductFetchResponse {
+    data: {
+        Product: Product
+    }
 }
