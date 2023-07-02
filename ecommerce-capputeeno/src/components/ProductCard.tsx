11926f681bb45bd58ab39a3ff0983843ee83fcc1
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-07-01 22:02:28 -0300

    feat: add navegação para página de produtos

--- ecommerce-capputeeno/src/components/ProductCard.tsx
+++ ecommerce-capputeeno/src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { formatPrice } from '@/utils/FormatPrice';
+import { useRouter } from 'next/navigation';
 import * as React from 'react';
 import { styled } from 'styled-components';
 
@@ -6,6 +7,7 @@ export interface ProductcardProps {
     image: string,
     title: string,
     price: number,
+    id: string,
 }
 
 const Card = styled.div`
@@ -13,16 +15,17 @@ const Card = styled.div`
     align-items:  center;
     justify-content: center;
     flex-direction: column;
-
+    
     background-color: rgba(255,255,255, 0.4);
     backdrop-filter: blur(10px);
     border-radius: 0px 0px 4px 4px;  
-
+    
     width: 256px;
-
+    
     img {
         width: 256px;
         height: 300px;
+        cursor: pointer;
     } 
 
     h3 {
@@ -60,11 +63,16 @@ const Card = styled.div`
 `
 
 export function Productcard(props: ProductcardProps) {
+    const router = useRouter()
     const price = formatPrice(props.price)
 
+    const handleNavigate = () => {
+        router.push(`/product?id=${props.id}`)
+    }
+
     return (
-        <Card>
-                <img src={props.image} />
+        <Card onClick={handleNavigate}>
+            <img src={props.image} />
             <div>
                 <h3>{props.title}</h3>
                 <div></div>
